0174a8b203e554e57dd32ca1a9518cf4c29425cd
CondimentCommander <64157688+CondimentCommander@users.noreply.github.com>
2022-03-16 07:13:51 -0400

    better operators + selections and more

--- ctext.js
+++ ctext.js
@@ -2,28 +2,12 @@
 import minimist from 'minimist';
 import fs from 'fs';
 
-import { operators } from './operators.js'
+import { intParseArgs, operators, splitArgs, resolveInput } from './operators.js'
 
 const argv = minimist(process.argv.slice(2));
 
 const inputList = argv._;
 const outputList = argv.o;
-
-const resolveInput = (input) => {
-	let text = input;
-	if (fs.existsSync(input)) {
-		try {
-			text = fs.readFileSync(input, 'utf-8');
-		} catch (err) {
-			console.error(err);
-		}
-	}
-	if (text[0] === "'" || text[0] === '"') {
-		text = text.substring(1);
-	}
-	text = text.replace(/\\n/mg, '\n');
-	return text;
-};
 
 const resolveOutputs = (outputs) => {
 	if (outputs === undefined) return [];
@@ -43,48 +27,88 @@ const writeOutput = async (output, text) => {
 	}
 };
 
+const evalInput = async (input, operator, argument) => {
+	return operator.fun(input, argument);
+};
+
 /* Main Function */
 const main = () => {
 	let inputs = inputList.map((input) => {return resolveInput(input)});
+	if (inputs.length === 0) inputs = [''];
 	let outputs = resolveOutputs(outputList);
 	const notOps = [
 		'o',
 		'_'
 	];
+	/* Find and parse all operator arguments */
 	let opsToApply = [];
 	Object.keys(argv).forEach((item) => {
-		if (!notOps.includes(item) && Object.keys(operators).includes(item)) {
-			let object = {
-				'op': item,
+		const parts = item.split('[');
+		let selection = [];
+		if (parts.length >= 2) {
+			selection = intParseArgs(splitArgs(parts[1].substring(0, parts[1].length)));
+		}
+		const name = parts[0];
+		if (!notOps.includes(name) && Object.keys(operators).includes(name)) {
+			const object = {
+				'name': name,
+				'selection': selection,
 				'arg': argv[item]
 			};
 			opsToApply.push(object);
 		}
 	});
+	/* Apply the operators */
 	let evaluated = inputs;
-	opsToApply.forEach((operator) => {
-		let op = operators[operator.op];
+	opsToApply.forEach((operatorArgument) => {
+		let op = operators[operatorArgument.name];
 		if (op.type === 'single') {
 			for (let i = 0; i < evaluated.length; i++) {
-				evaluated[i] = op.fun(evaluated[i], operator.arg);
+				if (operatorArgument.selection.length === 0 || operatorArgument.selection.includes(i)) {
+					evaluated[i] = op.fun(evaluated[i], operatorArgument.arg);
+				}
 			}
 		} else {
-			evaluated = op.fun(evaluated, operator.arg);
+			if (operatorArgument.selection.length === 0) {
+				evaluated = op.fun(evaluated, operatorArgument.arg);
+			} else {
+				let items = [];
+				evaluated.forEach((value, index) => {
+					if (operatorArgument.selection.includes(index)) {
+						items.push(value);
+					}
+				});
+				for (let i = 0; i < operatorArgument.selection.length; i++) {
+					evaluated.splice(operatorArgument.selection[i] - i, 1);
+				}
+				const operated = op.fun(items, operatorArgument.arg);
+				for (let i = 0; i < operated.length; i++) {
+					evaluated.splice(operatorArgument.selection[i], 0, operated[i]);
+				}
+			}
 		}
 	});
+	/* Pipe values to outputs */
 	evaluated.forEach((value, i) => {
 		if (argv.h !== true) {
-			console.log('"' + value + '"');
+			const maxLength = 150;
+			if (value.length <= maxLength) {
+				console.log('"' + value + '"');
+			} else {
+				console.log("String is larger than " + maxLength + " characters, will not print");
+			}
 		}
 		try {
-			if (outputs.length === evaluated.length) {
-				writeOutput(outputs[i], value);
-			} else if (outputs.length <= evaluated.length && i === 0) {
-				outputs.forEach((out) => {
-					writeOutput(out, evaluated[0]);
-				});
-			} else {
-				console.log("You have more values than outputs! No outputs were written");
+			if (outputs.length > 0) {
+				if (outputs.length === evaluated.length) {
+					writeOutput(outputs[i], value);
+				} else if (outputs.length <= evaluated.length && i === 0) {
+					outputs.forEach((out) => {
+						writeOutput(out, evaluated[0]);
+					});
+				} else {
+					console.log("You have more values than outputs! No outputs were written");
+				}
 			}
 		} catch (err) {
 			console.error(err);
--- operators.js
+++ operators.js
@@ -1,7 +1,23 @@
-var inputList = [];
+import fs from 'fs';
 
-const splitArgs = (args) => {
+export const splitArgs = (args) => {
 	return args.split(',');
+};
+
+export const resolveInput = (input) => {
+	let text = input;
+	if (fs.existsSync(input)) {
+		try {
+			text = fs.readFileSync(input, 'utf-8');
+		} catch (err) {
+			console.error(err);
+		}
+	}
+	if (text[0] === "\\") {
+		text = text.substring(1);
+	}
+	text = text.replace(/\\n/mg, '\n');
+	return text;
 };
 
 const linify = (text) => {
@@ -19,7 +35,7 @@ const textify = (lines) => {
 	return out;
 };
 
-const intParseArgs = (args) => {
+export const intParseArgs = (args) => {
 	for (let i = 0; i < args.length; i++) {
 		args[i] = parseInt(args[i]);
 	}
@@ -27,12 +43,62 @@ const intParseArgs = (args) => {
 };
 
 const box = (text, indexes) => {
-	let lines = linify(text);
-	let start = {line: indexes[0] - 1, col: indexes[1] - 1};
-	let end = {line: indexes[2] - 1, col: indexes[3] - 1};
+	const lines = linify(text);
+	const start = {line: indexes[0] - 1, col: indexes[1] - 1};
+	const end = {line: indexes[2] - 1, col: indexes[3] - 1};
 	let out = [];
 	for (let i = start.line; i < end.line; i++) {
 		out.push(lines[i].substring(start.col, end.col));
+	}
+	return out;
+};
+
+const replaceAt = (string, pos, size, text) => {
+	return string.substring(0, pos) + text + string.substring(pos + size, string.length);
+};
+
+const numEncodeMap = {
+	'a': 1,
+	'b': 2,
+	'c': 3,
+	'd': 4,
+	'e': 5,
+	'f': 6,
+	'g': 7,
+	'h': 8,
+	'i': 9,
+	'j': 10,
+	'j': 11,
+	'l': 12,
+	'm': 13,
+	'n': 14,
+	'o': 15,
+	'p': 16,
+	'q': 17,
+	'r': 18,
+	's': 19,
+	't': 20,
+	'u': 21,
+	'v': 22,
+	'w': 23,
+	'x': 24,
+	'y': 25,
+	'z': 26,
+}
+const numEncode = (string) => {
+	const lower = string.toLowerCase();
+	let out = [];
+	for (let i = 0; i < lower.length; i++) {
+		out.push(numEncodeMap[lower[i]]);
+	}
+	return out;
+};
+
+const numDecodeMap = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'j', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const numDecode = (nums) => {
+	let out = '';
+	for (let i = 0; i < nums.length; i++) {
+		out = out + (numDecodeMap[nums[i] - 1]);
 	}
 	return out;
 };
@@ -72,6 +138,18 @@ export const operators = {
 					out = input.toLowerCase();
 					break;
 				}
+				case 'title': {
+					out = input.toLowerCase();
+					const regex = /(\s|\.|\,)/mg;
+					const matches = out.matchAll(regex);
+					for (const match of matches) {
+						if (match.index !== out.length - 1) {
+							out = replaceAt(out, match.index + 1, 1, out[match.index + 1].toUpperCase());
+						}
+					}
+					out = replaceAt(out, 0, 1, out[0].toUpperCase());
+					break;
+				}
 			}
 			return out;
 		},
@@ -81,11 +159,19 @@ export const operators = {
 		'cat',
 		'Joins multiple text strings',
 		(inputs, argument) => {
-			let out = '';
-			inputs.forEach((item) => {
-				out = out + item;
-			});
-			return [out];
+			if (argument === true) {
+				let out = '';
+				inputs.forEach((item) => {
+					out = out + item;
+				});
+				return [out];
+			} else {
+				let out = [];
+				inputs.forEach((input) => {
+					out.push(input + resolveInput(argument));
+				});
+				return out;
+			}
 		},
 		'multi'
 	),
@@ -93,7 +179,7 @@ export const operators = {
 		'repeat',
 		'Repeats a string a certain amount of times',
 		(input, argument) => {
-			let times = parseInt(argument);
+			const times = parseInt(argument);
 			let out = '';
 			for (let i = 0; i < times; i++) {
 				out = out + input;
@@ -102,16 +188,16 @@ export const operators = {
 		},
 		'single'
 	),
-	'select': new Operator(
-		'select',
+	'sub': new Operator(
+		'sub',
 		'Obtains a substring of text',
 		(input, argument) => {
 			if (argument.includes(',')) {
-				let args = splitArgs(argument);
+				const args = splitArgs(argument);
 				args = intParseArgs(args);
 				return input.substring(args[0], args[1]);
 			} else if (argument.includes('-')) {
-				let args = argument.split('-');
+				const args = argument.split('-');
 				args = intParseArgs(args);
 				return input.substring(args[0], args[0] + args[1]);
 			} else {
@@ -120,19 +206,19 @@ export const operators = {
 		},
 		'single'
 	),
-	'selectbox': new Operator(
-		'selectbox',
+	'boxsub': new Operator(
+		'boxsub',
 		'Selects a substring using row and column indexes',
 		(input, argument) => {
 			if (argument.includes(',')) {
-				let args = splitArgs(argument);
+				const args = splitArgs(argument);
 				args = intParseArgs(args);
-				let selection = box(input, [args[0], args[1], args[2] + 1, args[3] + 1]);
+				const selection = box(input, [args[0], args[1], args[2] + 1, args[3] + 1]);
 				return textify(selection);
 			} else if (argument.includes('-')) {
-				let args = argument.split('-');
+				const args = argument.split('-');
 				args = intParseArgs(args);
-				let selection = box(input, [args[0], args[1], args[0] + args[2], args[1] + args[3]]);
+				const selection = box(input, [args[0], args[1], args[0] + args[2], args[1] + args[3]]);
 				return textify(selection);
 			} else {
 				return input;
@@ -145,9 +231,9 @@ export const operators = {
 		'Obtains the character at a position',
 		(input, argument) => {
 			if (input.includes(',')) {
-				let args = splitArgs(argument);
+				const args = splitArgs(argument);
 				args = intParseArgs(args);
-				let lines = linify(input);
+				const lines = linify(input);
 				return lines[args[0] - 1][args[1] - 1];
 			} else {
 				return input[parseInt(argument)];
@@ -159,9 +245,9 @@ export const operators = {
 		'rep',
 		'Replaces occurences of a string with another one',
 		(input, argument) => {
-			let args = splitArgs(argument);
-			let reg = new RegExp(args[0], 'mg');
-			return input.replace(reg, args[1]);
+			const args = splitArgs(argument);
+			const reg = new RegExp(resolveInput(args[0]), 'mg');
+			return input.replace(reg, resolveInput(args[1]));
 		},
 		'single'
 	),
@@ -169,7 +255,23 @@ export const operators = {
 		'weave',
 		'Interlaces multiple strings',
 		(inputs, argument) => {
-
+			const size = parseInt(argument);
+			let out = '';
+			let done = false;
+			let i = 0;
+			while (!done) {
+				done = true;
+				inputs.forEach((input) => {
+					if (i + size >= input.length) {
+						out = out + input.substring(i, input.length);
+					} else {
+						done = false;
+						out = out + input.substring(i, size);
+					}
+				});
+				i += size;
+			}
+			return [out];
 		},
 		'multi'
 	),
@@ -177,12 +279,119 @@ export const operators = {
 		'shrink',
 		'Removes characters from the beginning or end of text',
 		(input, argument) => {
-			let arg = parseInt(argument);
+			const arg = parseInt(argument);
 			if (arg < 0) {
 				return input.substring(0, input.length + arg);
 			} else {
 				return input.substring(arg, input.length);
 			}
+		},
+		'single'
+	),
+	'oneliner': new Operator(
+		'oneliner',
+		'Removes all newlines',
+		(input, argument) => {
+			console.log(argument);
+			const arg = (argument === 'true');
+			if (arg) {
+				return input.replace(/\n/mg, ' ');
+			} else {
+				return input.replace(/\n/mg, '');
+			}
+		},
+		'single'
+	),
+	'place': new Operator(
+		'place',
+		'Replaces text at a specified index',
+		(input, argument) => {
+			const args = splitArgs(argument);
+			const index = parseInt(args[0]);
+			const text = args[1];
+			if (args.length >= 3) {
+				return replaceAt(input, index, parseInt(args[2]), resolveInput(text));
+			} else {
+				return replaceAt(input, index, text.length, resolveInput(text));
+			}
+		},
+		'single'
+	),
+	'yell': new Operator(
+		'yell',
+		'Repeats letters',
+		(input, argument) => {
+			let out = '';
+			const size = parseInt(argument);
+			for (let i = 0; i < input.length; i++) {
+				if (input[i] == ' ') {
+					out = out + input[i];
+				} else {
+					for (let j = 0; j < size; j++) {
+						out = out + input[i];
+					}
+				}
+			}
+			return out;
+		},
+		'single'
+	),
+	'otp': new Operator(
+		'otp',
+		'One time pad cipher',
+		(input, argument) => {
+			const args = splitArgs(argument);
+			const key = numEncode(resolveInput(args[0]));
+			const encoded = numEncode(input);
+
+			const wrap = (value) => {
+				if (value > numDecodeMap.length) {
+					return value - numDecodeMap.length;
+				} else if (value <= 0) {
+					return numDecodeMap.length + value;
+				} else {
+					return value;
+				}
+			};
+
+			const mode = args[1];
+			let out = [];
+			for (let i = 0; i < encoded.length; i++) {
+				if (i >= key.length) {
+					out.push(encoded[i]);
+				} else {
+					if (mode === 'encode') {
+						out.push(wrap(encoded[i] + key[i] - 1));
+					} else {
+						out.push(wrap(encoded[i] - key[i] + 1));
+					}
+				}
+			}
+			return numDecode(out);
+		},
+		'single'
+	),
+	'help': new Operator(
+		'help',
+		'Gives help info',
+		(input, argument) => {
+			try {
+				const op = operators[argument];
+				return op.name + ': ' + op.desc;
+			} catch (err) {
+				console.log(err);
+				return "Operator does not exist!";
+			}
+		},
+		'single'
+	),
+	'insert': new Operator(
+		'insert',
+		'Inserts text at a position in another string',
+		(input, argument) => {
+			const args = splitArgs(argument);
+			const pos = parseInt(args[1]);
+			return input.substring(0, pos) + resolveInput(args[0]) + input.substring(pos, input.length);
 		},
 		'single'
 	)
